fb57f05cde3fbca6f5dba7eb9c73e0b7a97cf876
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-07-19 20:05:17 -0300

    structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header id="header">
+            <a className="logo" href="/">Plomería Bautista Ángel</a>
+            <nav>
+                <a href="#services">Servicios</a>
+                <a href="#about-us">Sobre nosotros</a>
+                <a href="#contact">Contactenos</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
